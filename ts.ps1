ffca61fe4901b697d192de5cc67088b462ff33ab
devi854 <79244706+devi854@users.noreply.github.com>
2023-09-28 22:41:02 +0530

    Create ts.ps1

---
+++ ts.ps1
@@ -0,0 +1,2 @@
+pbicli login --client-secret --clientId 647b60eb-374e-4af1-b7c7-439ba4913814 --clientSecret peA8Q~TGozmsgvf8dT3CF16mDlTVvOUuCufJ_aDF --tenant 0277ec7c-6485-4a45-9d8e-07a6941a7819
+pbicli dataset set-parameters --datasetId <DatasetId> --parameters "Server=data,Database=dev"
