b8357415c54fe366079f5b648f43842c20e032e0
devi854 <79244706+devi854@users.noreply.github.com>
2023-10-04 21:49:56 +0530

    Update update_dataset_parameters.ps1

--- update_dataset_parameters.ps1
+++ update_dataset_parameters.ps1
@@ -7,60 +7,44 @@ if (-not (Get-Module -Name MicrosoftPowerBIMgmt -ListAvailable)) {
 # update_dataset_parameters.ps1
 # update_dataset_parameters.ps1
 
-# Parameters
-$clientId = $env:POWER_BI_CLIENT_ID
-$clientSecret = $env:POWER_BI_CLIENT_SECRET
-$tenantId = $env:POWER_BI_TENANT_ID
-$workspaceName = "data of deals" # Update to the desired workspace name
-$datasetName = $env:POWER_BI_DATASET_NAME
+# Connect to the Power BI service
+Connect-PowerBIServiceAccount
 
-# Authenticate using Service Principal
-$secPassword = ConvertTo-SecureString $clientSecret -AsPlainText -Force
-$credential = New-Object PSCredential $clientId, $secPassword
+# Get the workspace ID and dataset ID (Replace with your workspace and dataset names)
+$workspaceName = "96b1ab18-cd5e-4e01-8ffd-8bf59651f1bf"
+$datasetName = "1e9f210c-d1b2-480c-8184-96c3938e1fef"
 
-# Log in with Service Principal credentials
-Login-PowerBIServiceAccount -ServicePrincipal -Credential $credential -Tenant $tenantId
+$workspace = Get-PowerBIWorkspace -Scope Organization | Where-Object { $_.Name -eq $workspaceName }
+$dataset = Get-PowerBIDataset -WorkspaceId $workspace.Id | Where-Object { $_.Name -eq $datasetName }
 
-# Get the Power BI workspace using the provided filter
-$workspace = Get-PowerBIWorkspace -Scope Organization -Filter "tolower(name) eq '$workspaceName'"
-
-if ($workspace -eq $null) {
-    Write-Host "Workspace '$workspaceName' not found."
+if ($workspace -eq $null -or $dataset -eq $null) {
+    Write-Host "Workspace or dataset not found."
     exit 1
 }
 
-# Get the dataset within the workspace
-$dataset = Get-PowerBIDataset -WorkspaceId $workspace.Id -Scope Organization | Where-Object Name -eq $datasetName
+# Create JSON object with update details
+$updateDetails = @(
+    @{
+        name = "EndDate"
+        newValue = "2020-06-08"
+    },
+    @{
+        name = "StartDate"
+        newValue = "2020-06-01"
+    }
+)
 
-if ($dataset -eq $null) {
-    Write-Host "Dataset '$datasetName' not found in workspace '$workspaceName'."
-    exit 1
-}
-
-# Create REST URL to update State parameter for the dataset
-$datasetParametersUrl = "https://api.powerbi.com/v1.0/myorg/groups/$($workspace.Id)/datasets/$($dataset.Id)/Default.UpdateParameters"
-
-# Define the parameter name and new value
-$parameterName = "myserver"
-$newParameterValue = "mydb"
-
-# Create JSON for the POST body to update dataset parameters
-$postBody = @{
-    updateDetails = @(
-        @{
-            name = $parameterName
-            newValue = $newParameterValue
-        }
-    )
+$body = @{
+    updateDetails = $updateDetails
 } | ConvertTo-Json
 
-# Invoke POST operation to update dataset parameters
-Invoke-RestMethod -Uri $datasetParametersUrl -Method Post -Headers @{
-    'Authorization' = "Bearer $((Get-PowerBIAccessToken).AccessToken)"
-} -Body $postBody -ContentType 'application/json'
+# Specify the URL for updating parameters
+$datasetId = $dataset.Id
+$datasetParametersUrl = "/datasets/$datasetId/Default.UpdateParameters"
 
-# If parameter updates change connection information (e.g., server path, database name),
-# you may need to patch datasource credentials before refreshing the dataset
+# Invoke the UpdateParameters operation
+Invoke-PowerBIRestMethod -Url $datasetParametersUrl -Method Post -Body $body -ContentType "application/json"
 
-# Log out to end the session (optional)
-# Logout-PowerBIServiceAccount
+# Refresh the dataset (Optional)
+# Invoke-PowerBIRestMethod -Url "/datasets/$datasetId/refreshes" -Method Post
+
