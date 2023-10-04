fd531ca423647d1ddf46167a440783d7b658941a
devi854 <79244706+devi854@users.noreply.github.com>
2023-10-04 20:41:41 +0530

    Update update_dataset_parameters.ps1

--- update_dataset_parameters.ps1
+++ update_dataset_parameters.ps1
@@ -4,6 +4,8 @@
 if (-not (Get-Module -Name MicrosoftPowerBIMgmt -ListAvailable)) {
     Install-Module -Name MicrosoftPowerBIMgmt -Force -Scope CurrentUser
 }
+# update_dataset_parameters.ps1
+
 # Parameters
 $clientId = $env:POWER_BI_CLIENT_ID
 $clientSecret = $env:POWER_BI_CLIENT_SECRET
@@ -16,7 +18,15 @@ $secPassword = ConvertTo-SecureString $clientSecret -AsPlainText -Force
 $credential = New-Object PSCredential $clientId, $secPassword
 
 # Get an Azure AD token using Service Principal credentials
-$token = Get-PowerBIAccessToken -ClientId $clientId -ClientSecret $clientSecret -TenantId $tenantId
+$tokenUrl = "https://login.microsoftonline.com/$tenantId/oauth2/token"
+$tokenBody = @{
+    grant_type    = "client_credentials"
+    client_id     = $clientId
+    client_secret = $clientSecret
+    resource      = "https://analysis.windows.net/powerbi/api"
+}
+$response = Invoke-RestMethod -Uri $tokenUrl -Method Post -Body $tokenBody
+$token = $response.access_token
 
 # Update these parameters for the target workspace and dataset
 $workspace = Get-PowerBIWorkspace -Name $workspaceName -Scope Organization -AccessToken $token
@@ -28,8 +38,8 @@ $datasetId = $dataset.Id
 $datasetParametersUrl = "https://api.powerbi.com/v1.0/myorg/groups/$workspaceId/datasets/$datasetId/Default.UpdateParameters"
 
 # Define the parameter name and new value
-$parameterName = "DevServer"
-$newParameterValue = "SQLServer"
+$parameterName = "sqlserver"
+$newParameterValue = "sqldatabase"
 
 # Create JSON for the POST body to update dataset parameters
 $postBody = @{
